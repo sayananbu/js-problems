a56555de4d3120384e4e890e196ca951854ad0a7
sayananbu <67857676+sayananbu@users.noreply.github.com>
2023-03-22 12:12:57 +0300

    Update 011-decrypt.js

--- problems/011-decrypt.js
+++ problems/011-decrypt.js
@@ -15,8 +15,20 @@
  * @param {string} secret
  * @returns {string}
  */
-function decrypt(secret) {
-    return undefined;
+function decrypt(secret, offset=1) {
+	secret=secret.toLocaleLowerCase()
+	let sentence = secret.split('')
+	let charcode = 0
+	let result = ''
+	sentence.forEach((value, index) => {
+		if(value !== ' '){
+			if(value !== 'z') charcode = value.charCodeAt()+1
+			else charcode = value.charCodeAt()-25
+			result += String.fromCharCode(charcode)
+		}
+		else result+= value
+	})
+    return result;
 }
 
 module.exports = decrypt;
