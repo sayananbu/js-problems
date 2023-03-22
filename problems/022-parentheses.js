c145b965b5fcd4105bf502f8a8438598600cb7e5
hyrule <67857676+sayananbu@users.noreply.github.com>
2023-03-23 00:36:03 +0300

    Update 022-parentheses.js

--- problems/022-parentheses.js
+++ problems/022-parentheses.js
@@ -16,7 +16,13 @@
  * @returns {boolean}
  */
 function parentheses(value) {
-    return undefined;
+	if(value.length === 0 || value.length < 2) return false
+	let stack = []
+	value.split('').forEach(item=>{
+		if(item === ')' && stack[stack.length-1] === '(') stack.pop()
+		else stack.push(item)
+	})
+    return stack.length === 0 ? true : false;
 }
 
 module.exports = parentheses;
