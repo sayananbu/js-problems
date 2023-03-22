7328ada9fafe7bf9587fa65c963c6568be25e02e
hyrule <67857676+sayananbu@users.noreply.github.com>
2023-03-22 23:27:32 +0300

    Update 019-search-anagrams.js

--- problems/019-search-anagrams.js
+++ problems/019-search-anagrams.js
@@ -11,8 +11,21 @@
  * @param {string} value
  * @returns {string}
  */
+const anagram = require('./018-anagram');
+const anagramCheck = require('./018-anagram')
 function searchAnagrams(value) {
-    return undefined;
+	if(value === '') return ''
+	let words = value.replaceAll(/[,.]/g,'').split(' ')
+	let anagrams = []
+	for (let i = 0; i < words.length; i++) {
+		for (let j = 0; j < words.length; j++) {
+			if(i === j) continue
+			if(anagramCheck(words[i],words[j])){
+				anagrams.push(words[i])
+			}
+		}
+	}
+    return anagrams.length > 0 ? anagrams.join([' ']) : ''
 }
 
 module.exports = searchAnagrams;
