eafe7f0847e9f742fda8515c822eff1d7813d069
hyrule <67857676+sayananbu@users.noreply.github.com>
2023-03-23 00:19:56 +0300

    Update 020-rle.js

--- problems/020-rle.js
+++ problems/020-rle.js
@@ -10,7 +10,51 @@
  * @returns {string}
  */
 function rle(value) {
-    return undefined;
+	if(value.length === 0) return ''
+	let count = 0
+	let letter = value[0]
+	let arr = value.split('')
+	let result = []
+	for (let i = 0; i < arr.length; i++) {
+		if(item === letter){
+			count++
+			if(arr.length === index+1){
+				let str = count > 1 ? count+letter : letter
+				result.push(str)
+			}
+		} 
+		else{
+			let str = count > 1 ? count+letter : letter
+			result.push(str)
+			count = 1
+			letter = item
+			if(arr.length === index+1){
+				let str = count > 1 ? count+letter : letter
+				result.push(str)
+			}
+		}
+		
+	}
+	// arr.forEach((item, index) => {
+	// 	if(item === letter){
+	// 		count++
+	// 		if(arr.length === index+1){
+	// 			let str = count > 1 ? count+letter : letter
+	// 			result.push(str)
+	// 		}
+	// 	} 
+	// 	else{
+	// 		let str = count > 1 ? count+letter : letter
+	// 		result.push(str)
+	// 		count = 1
+	// 		letter = item
+	// 		if(arr.length === index+1){
+	// 			let str = count > 1 ? count+letter : letter
+	// 			result.push(str)
+	// 		}
+	// 	}
+	// })
+    return result.join('');
 }
 
 module.exports = rle;
