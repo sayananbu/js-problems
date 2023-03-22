af3d80bcbec06818118e94f6d7d21b5d1b63e48d
sayananbu <67857676+sayananbu@users.noreply.github.com>
2023-03-22 13:52:38 +0300

    Update 012-password.js

--- problems/012-password.js
+++ problems/012-password.js
@@ -18,7 +18,27 @@
  * @returns {boolean}
  */
 function validatePassword(password) {
-    return undefined;
+	const upperCase = [65,90]
+	const lowerCase = [97, 122]
+	let upper, lower, digit
+	upper = lower = digit = false
+	let length = password.length >= 7
+	if(!length) return false
+	for(let letter of password){
+		if(!(upper && lower && digit)){
+			if(!upper && letter.charCodeAt() >= upperCase[0] && letter.charCodeAt() <= upperCase[1]){
+				upper = true
+				continue
+			}
+			if(!lower && letter.charCodeAt() >= lowerCase[0] && letter.charCodeAt() <= lowerCase[1]){
+				lower = true
+				continue
+			}
+			if(!digit && Number.isInteger(+letter)) digit = true
+		}
+		else break
+	}
+    return upper && lower && digit;
 }
 
 module.exports = validatePassword;
