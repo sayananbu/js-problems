c8e2f6a86801956ef1df3f80cb3f27d21b5b8ac1
sayananbu <67857676+sayananbu@users.noreply.github.com>
2023-03-30 13:43:42 +0300

    Update 031-circular-dependency.js

--- problems/031-circular-dependency.js
+++ problems/031-circular-dependency.js
@@ -21,28 +21,38 @@
  * @param {Object.<string, Array.<string>>} servicesMap
  * @returns {boolean}
  */
-function hasCircularDependency(servicesMap) {
-	let servicesValues = []
-	let fieldsNum = 0
-	for(let service in servicesMap){
-		servicesValues.push(...servicesMap[service])
-		fieldsNum++
+// function hasCircularDependency(servicesMap) {
+// 	let servicesValues = []
+// 	let fieldsNum = 0
+// 	for(let service in servicesMap){
+// 		servicesValues.push(...servicesMap[service])
+// 		fieldsNum++
+// 	}
+// 	removeDuplicates(servicesValues.sort())
+// 	if(servicesValues.length<fieldsNum || servicesValues.length<1) return false
+// 	return true
+// }
+// const removeDuplicates = function(nums){
+// 	if(nums.length<2) return nums;
+//     for(let i = 0; i<nums.length; i++){
+// 		if(i+1<nums.length) {
+// 			let index = nums.indexOf(nums[i], i+1)
+// 			if(index>=0){
+// 				nums.splice(index,1)
+// 				i--
+// 			}
+// 		}
+//     }
+// 	return nums;
+// }
+function hasCircularDependency(servicesMap){
+	let keys = Object.keys(servicesMap)
+	if(keys.length < 1) return false
+	let deps = new Set(Object.values(servicesMap).flat(Infinity))
+	if(keys.length !== deps.size) return false
+	for(let key of keys){
+		if(!deps.has(key)) return false
 	}
-	removeDuplicates(servicesValues.sort())
-	if(servicesValues.length<fieldsNum || servicesValues.length<1) return false
 	return true
-}
-const removeDuplicates = function(nums){
-	if(nums.length<2) return nums;
-    for(let i = 0; i<nums.length; i++){
-		if(i+1<nums.length) {
-			let index = nums.indexOf(nums[i], i+1)
-			if(index>=0){
-				nums.splice(index,1)
-				i--
-			}
-		}
-    }
-	return nums;
 }
 module.exports = hasCircularDependency;
